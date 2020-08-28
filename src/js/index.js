4cf70469006a8e21c86ad8020d7308a5fd932b00
Dragon <shalltear9@163.com>
2020-08-28 10:39:14 +0800

    master

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import $ from './modules/jquery.js';
+// import { cookie } from './modules/cookie';
+
+// $(function() {
+$('#btn').on('click', function() {
+        import ('./modules/cookie.js').then(function({ cookie }) {
+            console.log(cookie);
+        })
+    })
+    // })
