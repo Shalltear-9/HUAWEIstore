6264c43f8a377eb09ed391fc583f631c0020fb2e
Dragon <shalltear9@163.com>
2020-08-29 14:43:16 +0800

    v 1.0.4

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,30 @@
 import $ from './modules/jquery.js';
-// import { cookie } from './modules/cookie';
+
+// 导入jQuery插件时无需指定名称 不需要调用
+// 在改写时 需要在插件中引入jQuery作为模块
+import './modules/jquery-tabs.js';
+import './modules/jquery-md5.js'; // MD5插件，可以对字符串进行MD5算法加密
+import Swiper from './modules/swiper-bundle.js';
+
+// console.log($.md5('abc')); // MD5插件使用语法
+
 
 $(function() {
-    $('#btn').on('click', function() {
-        import ('./modules/cookie.js').then(function({ cookie }) {
-            console.log(cookie);
-        })
-    })
-})
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: true, //等同于以下设置
+        pagination: {
+            clickable: true,
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    for (let i = 0; i < mySwiper.pagination.bullets.length; i++) {
+        mySwiper.pagination.bullets[i].onmouseover = function() {
+            this.click();
+        };
+    }
+});
